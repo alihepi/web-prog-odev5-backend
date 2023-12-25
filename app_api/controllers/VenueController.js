c28a37e7e2700ec0163567b96388f029f98a2035
Ali Mutlu <95170323+alihepi@users.noreply.github.com>
2023-12-25 23:18:42 +0300

    Add files via upload

--- app_api/controllers/VenueController.js
+++ app_api/controllers/VenueController.js
@@ -20,18 +20,23 @@ var converter = (function () {
 })();
 
 const listVenues = async function (req, res) {
-    var lat = parseFloat(req.query.lat);
-    var long = parseFloat(req.query.long);
-
-    var point = {
-        type: "Point",
-        coordinates: [lat, long],
-    };
-    var geoOptions = {
-        distanceField: "dis",
-        spherical: true,
-    };
     try {
+        var lat = parseFloat(req.query.lat);
+        var long = parseFloat(req.query.long);
+
+        if (isNaN(lat) || isNaN(long)) {
+            throw new Error("Latitude and longitude are required and must be valid numbers.");
+        }
+
+        var point = {
+            type: "Point",
+            coordinates: [lat, long],
+        };
+        var geoOptions = {
+            distanceField: "dis",
+            spherical: true,
+        };
+
         const result = await Venue.aggregate([
             {
                 $geoNear: {
@@ -53,48 +58,43 @@ const listVenues = async function (req, res) {
         });
         createResponse(res, 200, venues);
     } catch (e) {
-        createResponse(res, 404, {
-            status: "Enlem ve Boylam zorunlu ve sifirdan farkli olmali",
-        });
+        createResponse(res, 400, { status: e.message });
     }
 }
 
 const addVenue = async (req, res) => {
-
-    const newVenue = {
+    const newVenue = new Venue({
         name: req.body.name,
         address: req.body.address,
         rating: req.body.rating,
         foodanddrink: req.body.foodanddrink,
         coordinates: req.body.coordinates,
         hours: req.body.hours,
-    };
+    });
 
     try {
-        await venue.collection.insertOne(newVenue);
-        createResponse(res, 200, newVenue);
+        await newVenue.save();
+        createResponse(res, 201, newVenue);
     } catch (error) {
-        createResponse(res, 404, { status: "Mekan Eklenmedi..." });
+        createResponse(res, 500, { status: "Error adding venue." });
     }
 };
 
 const getVenue = async function (req, res) {
     try {
-        await Venue.findById(req.params.venueid).exec().then(function (venue) {
-            createResponse(res, 200, venue);
-        });
+        const venue = await Venue.findById(req.params.venueid).exec();
+        createResponse(res, 200, venue);
     } catch (error) {
-        createResponse(res, 404, { status: "Boyle bir mekan yok!" });
+        createResponse(res, 404, { status: "Venue not found." });
     }
-    //createResponse(res, 200, {status: "Basarili"});
 }
 
 const updateVenue = function (req, res) {
-    createResponse(res, 200, { status: "Basarili" });
+    createResponse(res, 200, { status: "Successful" });
 }
 
 const deleteVenue = function (req, res) {
-    createResponse(res, 200, { status: "Basarili" });
+    createResponse(res, 200, { status: "Successful" });
 }
 
 module.exports = {
