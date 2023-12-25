da32383824299dedb7318fba81b16b09f1189d65
Ali Mutlu <95170323+alihepi@users.noreply.github.com>
2023-12-26 00:10:08 +0300

    Update VenueController.js

--- app_api/controllers/VenueController.js
+++ app_api/controllers/VenueController.js
@@ -1,106 +1,97 @@
-var mongoose = require("mongoose");
-var Venue = mongoose.model("venue");
+var mongoose=require("mongoose");
+var Venue =mongoose.model("venue");
 
-const createResponse = function (res, status, content) {
-    res.status(status).json(content);
-}
-
-var converter = (function () {
-    var earthRadius = 6371;
-    var radian2Kilometer = function (radian) {
+var converter=(function(){
+    var earthRadius=6371; //km
+    var radian2Kilometer =function (radian){
         return parseFloat(radian * earthRadius);
     };
-    var kilometer2Radian = function (distance) {
-        return parseFloat(distance / earthRadius);
+    var kilometre2Radian=function(distance){
+        return parseFloat(distance/earthRadius);
     };
-    return {
+    return{
         radian2Kilometer,
-        kilometer2Radian,
+        kilometre2Radian,
     };
 })();
 
-const listVenues = async function (req, res) {
-    var lat = parseFloat(req.query.lat);
-    var long = parseFloat(req.query.long);
-
-    var point = {
-        type: "Point",
-        coordinates: [lat, long],
+const createResponse=function(res,status,content){
+    res.status(status).json(content);
+}
+const listVenues= async function(req,res){
+    var lat =parseFloat(req.query.lat);
+    var long=parseFloat(req.query.long);
+    var point={
+        type:"Point",
+        coordinates:[lat,long],
     };
-    var geoOptions = {
-        distanceField: "dis",
-        spherical: true,
+    var geoOptions={
+        distanceField:"dis",
+        spherical:true,
     };
-    try {
-        const result = await Venue.aggregate([
+    try{
+        const result= await Venue.aggregate([
             {
-                $geoNear: {
-                    near: point,
+                $geoNear:{
+                    near:point,
                     ...geoOptions,
                 },
             },
         ]);
-
-        const venues = result.map((venue) => {
-            return {
-                distance: converter.kilometer2Radian(venue.dis),
-                name: venue.name,
-                address: venue.address,
-                rating: venue.rating,
-                foodanddrink: venue.foodanddrink,
-                id: venue._id,
+        const venues=result.map((venue) => {
+            return{
+                distance:converter.kilometre2Radian(venue.dis),
+                name:venue.name,
+                address:venue.address,
+                rating:venue.rating,
+                foodanddrink:venue.foodanddrink,
+                id:venue._id,
             };
         });
-        createResponse(res, 200, venues);
-    } catch (e) {
-        createResponse(res, 404, {
-            status: "Enlem ve Boylam zorunlu ve sifirdan farkli olmali",
+        createResponse(res,200,venues);
+
+    }
+    catch(e){
+        createResponse(res,404,{
+            e,
         });
     }
+
+};
+const addVenue=function(req,res){
+    createResponse(res,200,{"status":"Başarılı"});
 }
+const getVenue= async function(req,res){
+    try{
+        await Venue.findById(req.params.venueid).exec()
+        .then(function(venue){
+            createResponse(res,200,venue);
 
-const addVenue = async (req, res) => {
+        });
 
-    const newVenue = {
-        name: req.body.name,
-        address: req.body.address,
-        rating: req.body.rating,
-        foodanddrink: req.body.foodanddrink,
-        coordinates: req.body.coordinates,
-        hours: req.body.hours,
-    };
+    }catch(error){
+        createResponse(res,404,{status:"Böyle Bir mekan yok"});
 
-    try {
-        await venue.collection.insertOne(newVenue);
-        createResponse(res, 200, newVenue);
-    } catch (error) {
-        createResponse(res, 404, { status: "Mekan Eklenmedi..." });
     }
 };
-
-const getVenue = async function (req, res) {
-    try {
-        await Venue.findById(req.params.venueid).exec().then(function (venue) {
-            createResponse(res, 200, venue);
+const updateVenue=function(req,res){
+    createResponse(res,200,{"status":"Başarılı"});
+}
+const deleteVenue= async function(req,res){
+    try{
+        await Venue.findByIdAndDelete(req.params.venueid).exec()
+        .then(function(venue){
+            createResponse(res,200,venue)
         });
-    } catch (error) {
-        createResponse(res, 404, { status: "Boyle bir mekan yok!" });
+
+    }catch(error){
+        createResponse(res,404,{status:"Böyle Bir mekan yok"});
     }
-    //createResponse(res, 200, {status: "Basarili"});
 }
-
-const updateVenue = function (req, res) {
-    createResponse(res, 200, { status: "Basarili" });
-}
-
-const deleteVenue = function (req, res) {
-    createResponse(res, 200, { status: "Basarili" });
-}
-
-module.exports = {
+module.exports={
     listVenues,
     addVenue,
-    getVenue,
     updateVenue,
     deleteVenue,
+    getVenue
 }
